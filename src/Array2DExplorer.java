c05061ff4ec5c5eab90be774a21754deb07cebe1
Michael George <georgem@wlwv.k12.or.us>
2020-02-18 08:55:58 -0800

    ah!

---
+++ src/Array2DExplorer.java
@@ -0,0 +1,37 @@
+public class Array2DExplorer {
+
+  public int minRow(int[] nums, int row)
+    nums.row;
+    int min;
+
+  public int[] colMaxs(int [][] matrix)
+    int colMaxs;
+
+  public int[] allRowSums (int[][]data)
+    array.row;
+    row return sum;
+
+  public double[] averageCol(int[][]nums)
+    column.average;
+    return array[]average;
+
+  public int smallEven(int[][]matrix)
+    index[]min;
+
+  public static int biggestRow(int[]nums)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
